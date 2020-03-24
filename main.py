19fb904d2b20dfd1b87786d086f84bda394b2626
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-03-24 19:41:46 +0530

    Added age and height display and buttons

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.config import Config
+from kivy.core.text.markup import MarkupLabel
 
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
@@ -20,6 +22,54 @@ KV = '''
 #:import FadeTransition kivy.uix.screenmanager.FadeTransition
 #:set color_deep_purple [0.36862745098,0.20784313725 ,0.69411764705,1]
 #:set color_deep_purple_dark [0.270588235,0.152941176 ,0.62745098,1]
+#:set color_deep_purple_light [0.701960784,0.615686275 ,0.858823529,1]
+<Custom@FloatLayout>:
+	Label:
+		text: "[b][color=b39ddb]Age[/color][/b]"
+		markup: True
+		font_size: self.size[0]/19
+		pos_hint: {'center_x':0.25, 'center_y': 0.97}
+	Label:
+		text: "[b][color=b39ddb]Height(cms)[/color][/b]"
+		markup: True
+		font_size: self.size[0]/19
+		pos_hint: {'center_x':0.75, 'center_y': 0.97}
+	Label:
+		id: age
+		text: "[b][color=b39ddb]21[/color][/b]"
+		markup: True
+		font_size: self.size[0]/10
+		pos_hint: {'center_x':0.25, 'center_y': 0.9}
+	Label:
+		id: height
+		text: "[b][color=b39ddb]185[/color][/b]"
+		markup: True
+		font_size: self.size[0]/10
+		pos_hint: {'center_x':0.75, 'center_y': 0.9}
+	MDIconButton:
+		icon: "minus"
+		pos_hint: {'center_x':0.15, 'center_y': 0.83}
+		theme_text_color: "Custom"
+		text_color: color_deep_purple_light
+		on_press: root.age_decrement()
+	MDIconButton:
+		icon: "plus"
+		pos_hint: {'center_x':0.35, 'center_y': 0.83}
+		theme_text_color: "Custom"
+		text_color: color_deep_purple_light
+		on_press: root.age_increment()
+	MDIconButton:
+		icon: "minus"
+		pos_hint: {'center_x':0.65, 'center_y': 0.83}
+		theme_text_color: "Custom"
+		text_color: color_deep_purple_light
+		on_press: root.height_decrement()
+	MDIconButton:
+		icon: "plus"
+		pos_hint: {'center_x':0.85, 'center_y': 0.83}
+		theme_text_color: "Custom"
+		text_color: color_deep_purple_light
+		on_press: root.height_increment()
 <BoxContentForBottomSheetCustomScreenList>
     orientation: "vertical"
     padding: dp(10)
@@ -93,17 +143,7 @@ Screen:
 										pos: self.size[1]/60, 0 + self.size[1]/60
 										size: self.size[0] - self.size[1]/15 + self.size[1]/30, self.size[1]/4 - self.size[1]/60
 										radius: [20]
-								FloatLayout:
-									Label:
-										text: "[b][color=b39ddb]Age[/color][/b]"
-										markup: True
-										font_size: self.size[0]/19
-										pos_hint: {'center_x':0.25, 'center_y': 0.97}
-									Label:
-										text: "[b][color=b39ddb]Height(cms)[/color][/b]"
-										markup: True
-										font_size: self.size[0]/19
-										pos_hint: {'center_x':0.75, 'center_y': 0.97}
+								Custom:
 
 					Screen:
 						name: "2"
@@ -129,7 +169,43 @@ class ContentNavigationDrawer(BoxLayout):
     screen_manager = ObjectProperty()
     nav_drawer = ObjectProperty()
 
+class Custom(FloatLayout):
+	def __init__(self, **kwargs):
+		super(Custom, self).__init__(**kwargs)
+		self._height = "[b][color=b39ddb]185[/color][/b]"
+		self._age = "[b][color=b39ddb]21[/color][/b]"
 
+	def height_increment(self):
+		self._height = MarkupLabel(self._height).markup
+		self._height = int(self._height[2])
+		self._height += 1
+		self._height = f'[b][color=b39ddb]{self._height}[/color][/b]'
+		self.ids.height.text = self._height
+
+	def height_decrement(self):
+		self._height = MarkupLabel(self._height).markup
+		self._height = int(self._height[2])
+		if self._height>0:
+			self._height -= 1
+		self._height = f'[b][color=b39ddb]{self._height}[/color][/b]'
+		self.ids.height.text = self._height
+
+	def age_increment(self):
+		self._age = MarkupLabel(self._age).markup
+		self._age = int(self._age[2])
+		self._age += 1
+		self._age = f'[b][color=b39ddb]{self._age}[/color][/b]'
+		self.ids.age.text = self._age
+
+	def age_decrement(self):
+		self._age = MarkupLabel(self._age).markup
+		self._age = int(self._age[2])
+		if self._age>0:
+			self._age -= 1
+		self._age = f'[b][color=b39ddb]{self._age}[/color][/b]'
+		self.ids.age.text = self._age
+
+	
 class MainApp(MDApp):
 	def build(self):
 		app = MDApp.get_running_app()
@@ -147,6 +223,7 @@ class MainApp(MDApp):
 		bs_menu.add_item("Made by Abhay Maurya", lambda x: None, icon="dev-to",)
 		bs_menu.add_item("using Python!", lambda x: None, icon="language-python",)
 		bs_menu.open()
+	
 	# @run_on_ui_thread
 	# def statusbar(self,color):
 	# 	window = activity.getWindow()
